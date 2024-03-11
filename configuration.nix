59df029efdac1fb739309f61ab5a7b1b5f15eb95
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2024-03-11 11:51:52 -0400

    Added support for hardware-configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,9 @@
 #!/usr/bin/nixos-rebuild
 {config, pkgs, ...}:
 
-## Upstream configurations
 {
-  imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-plasma5.nix> ];
+  ## Upstream configurations
+  imports = [
+    ./hardware-configuration.nix ## Run `nixos-generate-config` to update this file.
+  ];
 }
