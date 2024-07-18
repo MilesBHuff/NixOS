e434414543f0a4f67b12faa6b83f6d5842e61fff
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2024-07-18 12:52:18 -0400

    Switched to ZFS

--- mounts/pc.nix
+++ mounts/pc.nix
@@ -1,98 +1,17 @@
 #!/usr/bin/env nix eval -f
 {config, pkgs, ...}: {
-    #TODO: Set up disk encryption.
-    #TODO: Use ZFS instead of btrfs.
-    #TODO: Set SSD/HDD options dynamically.  `btrfs` already automatically sets SSD options for SSDs, but it doesn't set `autodefrag` for HDDs.
     #WARN: btrfs and zfs do poorly with access time updates, which actually trigger a full CoW, which can dramatically increase space usage if the files in-question have previously been snapshotted!  Accordingly, `atime` and `relatime` should *not* be used on btrfs or zfs if you plan to make use of snapshotting.
     #NOTE: `lazytime` affects `mtime` and `ctime`, too -- not just `atime`.  Accordingly, it can still be beneficial in tandem with `noatime`.
     fileSystems = {
 
         ################################################################################
-        ## BIOS DEVICES                                                               ##
+        ## IMPERSISTENT DEVICES                                                       ##
         ################################################################################
 
-        "/boot/EFI" = {
-            depends = [ "/" ];
-            device = "/dev/disk/by-label/ESP";
-            fsType = "vfat";
-            options = [
-                "iocharset=utf8" "tz=UTC" ## Will not be used by Windows, so making it maximally Linux-friendly.
-                "sync" "flush" ## FAT has no journalling, so all writes should be done synchronously to help ensure data integrity.  Write issues here can prevent booting!
-                "lazytime" "noatime"
-            ];
-        };
-        "/boot/EFI.bak" = {
-            depends = [ "/" ];
-            device = "/dev/disk/by-label/ESP.bak";
-            fsType = "vfat";
-            options = [
-                "iocharset=utf8" "tz=UTC" ## Will not be used by Windows, so making it maximally Linux-friendly.
-                "sync" "flush" ## FAT has no journalling, so all writes should be done synchronously to help ensure data integrity.  Write issues here can prevent booting!
-                "lazytime" "noatime"
-            ];
-        };
-
-        ################################################################################
-        ## SYSTEM DEVICES                                                             ##
-        ################################################################################
-
-        "/" = { #TODO: Set up a way to easily replace this with a new subvolume, for occasional impersistence.  I'd like to impersist root every time NixOS has a major update.
-            device = "/dev/disk/by-label/NixOS";
-            fsType = "btrfs";
-            options = [
-                "subvol=@root"
-                "compress=zstd:1"
-                "lazytime" "noatime"
-            ];
-        };
-        "/boot" = {
-            depends = [ "/" ];
-            device = "/dev/disk/by-label/NixOS";
-            fsType = "btrfs";
-            options = [
-                "subvol=@boot"
-                "compress=zstd:1"
-                "lazytime" "noatime"
-            ];
-        };
-        "/boot/.snapshots" = {
-            depends = [ "/" "/boot" ];
-            device = "/dev/disk/by-label/NixOS";
-            fsType = "btrfs";
-            options = [
-                "subvol=@boot/snapshots"
-                "ro"
-            ];
-        };
-        "/nix" = {
-            depends = [ "/" ];
-            device = "/dev/disk/by-label/NixOS";
-            fsType = "btrfs";
-            options = [
-                "subvol=@nix-store"
-                "compress=zstd:1"
-                "lazytime" "noatime"
-            ];
-        };
-        "/etc/nix" = {
-            depends = [ "/" ];
-            device = "/dev/disk/by-label/NixOS";
-            fsType = "btrfs";
-            options = [
-                "subvol=@nix-conf"
-                "compress=zstd:1"
-                "lazytime" "noatime"
-            ];
-        };
-        "/var/swap" = {
-            depends = [ "/" ];
-            device = "/dev/disk/by-label/NixOS";
-            fsType = "btrfs";
-            options = [
-                "subvol=@swap"
-                "nodatacow" ## Mandatory for swapfiles
-                "lazytime" "noatime"
-            ];
+        "/" = { #TODO: Make this automatically roll back to an empty snapshot at every boot, for impermanence.
+            device = "rpool/system";
+            fsType = "zfs";
+            options = [ "lazytime" ];
         };
         "/tmp" = {
             depends = [ "/" ];
@@ -103,76 +22,61 @@
                 "lazytime" "noatime"
             ];
         };
-        "/.snapshots" = {
-            depends = [ "/" ];
-            device = "/dev/disk/by-label/NixOS";
-            fsType = "btrfs";
-            options = [
-                "subvol=@snapshots"
-                "ro"
-            ];
-        };
 
         ################################################################################
-        ## USER/SERVICE DEVICES                                                       ##
+        ## SEMI-PERSISTENT DEVICES                                                    ##
         ################################################################################
 
-        "/usr/local" = {
+        "/boot/efi" = { ## https://docs.zfsbootmenu.org/en/v2.3.x/general/mdraid-esp.html
             depends = [ "/" ];
-            device = "/dev/disk/by-label/Local";
-            fsType = "btrfs";
+            device = "/dev/md/esp";
+            fsType = "vfat";
             options = [
-                "subvol=@usr"
-                "compress=zstd:1"
+                "iocharset=utf8" "tz=UTC" ## Will not be used by Windows, so making it maximally Linux-friendly.
+                "sync" "flush" ## FAT has no journalling, so all writes should be done synchronously to help ensure data integrity.  Write issues here can prevent booting!
                 "lazytime" "noatime"
             ];
         };
-        "/usr/local/.snapshots" = {
-            depends = [ "/" "/usr/local" ];
-            device = "/dev/disk/by-label/NixOS";
-            fsType = "btrfs";
-            options = [
-                "subvol=@local/snapshots"
-                "ro"
-            ];
+        "/boot" = {
+            depends = [ "/" ];
+            device = "/rpool/@/system/boot";
+            fsType = "zfs";
+            options = [ "lazytime" ];
+        };
+        "/var" = {
+            depends = [ "/" ];
+            device = "rpool/@/system/var";
+            fsType = "zfs";
+            options = [ "lazytime" ];
+        };
+        "/nix" = {
+            depends = [ "/" ];
+            device = "/rpool/@/nix";
+            fsType = "zfs";
+            options = [ "lazytime" ];
+        };
+
+        ################################################################################
+        ## PERSISTENT DEVICES                                                         ##
+        ################################################################################
+
+        "/.persist" = {
+            depends = [ "/" ];
+            device = "/rpool/@/persist";
+            fsType = "zfs";
+            options = [ "lazytime" ];
         };
         "/srv" = {
             depends = [ "/" ];
-            device = "/dev/disk/by-label/Local";
-            fsType = "btrfs";
-            options = [
-                "subvol=@srv"
-                "compress=zstd:1"
-                "lazytime" "noatime" #NOTE: Consider `atime` if you run an FTP server and aren't using btrfs snapshots.
-            ];
-        };
-        "/srv/.snapshots" = {
-            depends = [ "/" "/srv" ];
-            device = "/dev/disk/by-label/NixOS";
-            fsType = "btrfs";
-            options = [
-                "subvol=@local/snapshots"
-                "ro"
-            ];
+            device = "/rpool/@/persist/srv";
+            fsType = "zfs";
+            options = [ "lazytime" ];
         };
         "/home" = {
             depends = [ "/" ];
-            device = "/dev/disk/by-label/Local";
-            fsType = "btrfs";
-            options = [
-                "subvol=@home"
-                "compress=zstd:1"
-                "lazytime" "noatime" #NOTE: Consider `relatime` if not using btrfs snapshots, because access time is needed for certain backup programs and email clients to work right.
-            ];
-        };
-        "/home/.snapshots" = {
-            depends = [ "/" "/home" ];
-            device = "/dev/disk/by-label/NixOS";
-            fsType = "btrfs";
-            options = [
-                "subvol=@local/snapshots"
-                "ro"
-            ];
+            device = "/rpool/@/persist/home";
+            fsType = "zfs";
+            options = [ "lazytime" ];
         };
 
         ################################################################################
@@ -181,13 +85,15 @@
 
         "/media/games" = {
             depends = [ "/" ];
-            device = "/dev/disk/by-label/Games";
-            fsType = "btrfs";
-            options = [
-                "subvol=@games"
-                "compress=zstd:1"
-                "lazytime" "noatime"
-            ];
+            device = "/xpool/@/games";
+            fsType = "zfs";
+            options = [ "lazytime" "noauto" "user" ];
+        };
+        "/media/backups" = {
+            depends = [ "/" ];
+            device = "/kpool/@";
+            fsType = "zfs";
+            options = [ "lazytime" "noauto" ];
         };
     };
 
@@ -195,22 +101,23 @@
     ## SWAP DEVICES                                                               ##
     ################################################################################
 
+    #TODO: Create the `/swap` directory if it doesn't exist.
     #TODO: Create/delete swapfiles as needed (increments of 1GiB), so that there is always at leat 1GiB of free swap and less than 2GiB of free swap.
     #TODO: Dynamically create a `0.swp` file of a size equivalent to system RAM, for hibernation.
     swapDevices = [{
-        device = "/var/swap/1.swp";
+        device = "/swap/1.swp";
         priority = 99;
         size = 1024; #MiB
     } {
-        device = "/var/swap/2.swp";
+        device = "/swap/2.swp";
         priority = 98;
         size = 1024; #MiB
     } {
-        device = "/var/swap/3.swp";
+        device = "/swap/3.swp";
         priority = 97;
         size = 1024; #MiB
     } {
-        device = "/var/swap/4.swp";
+        device = "/swap/4.swp";
         priority = 96;
         size = 1024; #MiB
     }];
