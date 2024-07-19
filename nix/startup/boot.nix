3befad2482886183a1ff4b52c7ecafeff394a59a
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2024-07-19 07:53:03 -0400

    Add additional plans for boot

--- nix/startup/boot.nix
+++ nix/startup/boot.nix
@@ -2,4 +2,6 @@
 {config, pkgs, lib, ...}: {
     #TODO:  ZFSBootMenu in ESP
     #TODO:  systemd-boot for ZFS
+    #TODO:  rollback `/` to blank snapshot for impersistence
+    #TODO:  after system regenerates, copy `/.persist/` to `/`
 }
