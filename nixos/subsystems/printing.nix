bf97bf1211038557a049a24727d7713a034c188c
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2024-07-19 23:02:33 -0400

    Remove irrelevant comments

--- nixos/subsystems/printing.nix
+++ nixos/subsystems/printing.nix
@@ -1,8 +1,5 @@
 #!/usr/bin/env nix eval -f
 {config, pkgs, lib, var, ...}: {
     services.printing.enable = true;
-    environment.systemPackages = environment.systemPackages ++ [
-        #TODO: Add chrony
-    ];
-    #TODO: Set up NTS
+    environment.systemPackages = environment.systemPackages ++ [];
 }
