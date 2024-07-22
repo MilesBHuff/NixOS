be5d4e76ca55618a745adee66674f927f917c26b
Miles B Huff <MilesBHuff@users.noreply.github.com>
2024-07-22 11:08:27 -0400

    Added another TODO

--- nixos/subsystems/time.nix
+++ nixos/subsystems/time.nix
@@ -2,6 +2,7 @@
 {config, pkgs, lib, var, ...}: {
     time.timeZone = "America/New_York";
     services.chrony.enable = true;
+    #TODO: Disable RTC-related settings in Chrony if the system lacks an RTC (like Raspberry Pi 4s)
     #TODO: Create directories needed by `/etc/chrony/chrony.conf`.
     #TODO: Figure out if we need `hwclock`;  if we do, ensure that its systemd services are enabled.
 }
