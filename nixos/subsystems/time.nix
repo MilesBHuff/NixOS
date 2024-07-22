425345ff1a78852d50048335c6c690d530b64115
Miles B Huff <MilesBHuff@users.noreply.github.com>
2024-07-22 11:02:59 -0400

    Added a TODO

--- nixos/subsystems/time.nix
+++ nixos/subsystems/time.nix
@@ -2,5 +2,6 @@
 {config, pkgs, lib, var, ...}: {
     time.timeZone = "America/New_York";
     services.chrony.enable = true;
+    #TODO: Create directories needed by `/etc/chrony/chrony.conf`.
     #TODO: Figure out if we need `hwclock`;  if we do, ensure that its systemd services are enabled.
 }
