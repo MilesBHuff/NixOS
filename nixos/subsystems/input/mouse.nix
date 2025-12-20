1a572ad9846279ab227ff2d31163bfc4f4e84529
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2025-12-19 20:47:38 -0500

    Coalesced input, began planning for mouse

---
+++ nixos/subsystems/input/mouse.nix
@@ -0,0 +1,9 @@
+#!/usr/bin/env nix eval -f
+{config, pkgs, lib, var, ...}: {
+    #TODO
+    ## Middle-click should not copy/paste; it should enable toggle-scroll.
+    ## Tap-to-touch should be disabled for one finger, do a right-click for two fingers, and do a middle-click for three fingers.
+    ## Acceleration should be enabled.
+    ## Evdev and Synaptics should be configured for X.
+    ## Libinput should be configured for Wayland.
+}
