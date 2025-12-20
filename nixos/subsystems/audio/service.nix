6322abcaee459b5ac4ebfea0725b82b0a438ec44
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2025-12-19 22:34:08 -0500

    Added `default.pa`

--- nixos/subsystems/audio/service.nix
+++ nixos/subsystems/audio/service.nix
@@ -84,56 +84,182 @@
     };
 
     ## Settings: Pulseaudio (no longer used; kept for historical reasons)
-    services.pulseaudio.daemon.config = {
+    let
+        tsched = 1; ## `1` to reduce latency and power-consumption; `0` for compatibility.
+    in {
+        services.pulseaudio = {
+            extraConfig = ''
+                #!/usr/bin/pulseaudio -nF
+                ## This only runs when PulseAudio is started per-user (not system-wide).
+                .fail
 
-        ## Memory
-        lock-memory = true; ## Prevents paging out audio-related memory.
-        enable-memfd = true; ## Modern memory architecture.
-        enable-shm = true; ## Legacy memory architecture. Used if memfd isn't available.
-        # shm-size-bytes = 0; ## 0 == auto (64M)
+                ################################################################################
+                ## M E T A D A T A                                                            ##
+                ################################################################################
 
-        ## Scheduling
-        cpu-limit = false;
-        high-priority = true; ## Ignored if RT.
-        nice-level = -19; ## Ignored if RT.
-        realtime-scheduling = true;
-        realtime-priority = 40;
-        # tsched = 0; ## If `1`, can cause dropouts under load. Not present on newer versions of Pulseaudio.
-        default-fragments = 2; ## Should be greater than 1 but also as low as possible. Increase only if you experience playback issues. 2x4=8ms, which is about 0.5fps; this means that Pulseaudio won't be a cause of A/V desynchronization.
-        default-fragment-size-msec = 4; ## Going too far below about 5 can cause issues with quality (metalicness, ringing).
-        enable-deferred-volume = true; ## Avoids harsh changes mid-buffer.
-        deferred-volume-safety-margin-usec = 3000; ## Must be large-enough to handle timing errors, yet smaller than default-fragment-size-msec.
-        deferred-volume-extra-delay-usec = 0; ## Should be 0 unless you're working around hardware issues.
-        scache-idle-time = 60; ## How long to cache audio. Reduces disk reads.
-        exit-idle-time = 1; ## Affects `avoid-resampling`; should be low so that content plays more-often in its native rate. Consider adding a 1s delay between tracks in your audio player to take full advantage of automatic sample-rate switching.
+                ## Use /usr/share/application/*.desktop files as a source of information about streams
+                load-module module-augment-properties
 
-        ## Sampling
-        avoid-resampling = true; ## Locks Pulseaudio into the sample rate of the first track it plays.
-        default-sample-format = "float32le"; ## Not using 32+ bits or floats means deleting entire bits when scaling volume.
-        default-sample-rate = 48000; ## Best conventional option for math reasons.
-        alternate-sample-rate = 44100; ## CD-quality audio. Has to be supported because it's extremely common.
-        resample-method = "soxr-vhq";
+                ## Support intended roles
+                load-module module-intended-roles
 
-        ## Channels
-        enable-remixing = true;
-        remixing-use-all-sink-channels = true; ## Ensures surround-sound sats aren't sitting there idle during stereo playback.
-        remixing-produce-lfe = false; ## Keep bass stereo and let the speakers sort it out.
-        remixing-consume-lfe = true;
-        lfe-crossover-freq = 120; ## 120Hz is the official standard for LFE XO; we have to match it for playback to be accurate. The only reason to break the 120Hz standard is when you're generating your own LFE channel, which we aren't doing.
-        default-sample-channels = 2;
-        # default-channel-map = "side-left,side-right"; ## Whether speakers or headphones, I always have my sources at 180°. With `enable-remixing = true`, front channels (99% of everything) should move to side without issue, while surround will remap more-gracefully than it would to front.
-        default-channel-map = "front-left,front-right" ## The canonical option. Ensures nothing breaks, but not as literally correct.
+                ################################################################################
+                ## P R E - C O N F I G U R E                                                  ##
+                ################################################################################
 
-        ## Misc
-        rescue-streams = true; ## Prevents streams from sticking with dead sinks.
-        flat-volumes = false; ## Misguided behavior; leave disabled.
+                ## Restore volumes
+                load-module module-card-restore
+                load-module module-device-restore
+                load-module module-stream-restore
 
-        ## Logging
-        log-target = "auto";
-        log-level = "notice";
-        log-meta = false;
-        log-time = true;
-        log-backtrace = 0;
+                ## Automatically switch to new destinations
+                load-module module-switch-on-port-available
+                # load-module module-switch-on-connect
+
+                ################################################################################
+                ## L O A D   S I N K S                                                        ##
+                ################################################################################
+
+                ## If udev, load drivers per present hardware; else, load staticly
+                ## `tsched=1` uses less power and wastes less latency.
+                ## `tsched=0` works with more devices.
+                .ifexists module-udev-detect.so
+                load-module module-udev-detect tsched=${tsched}
+                .else
+                load-module module-detect tsched=${tsched}
+                .endif
+
+                ## UNIX support
+                .ifexists module-esound-protocol-unix.so
+                load-module module-esound-protocol-unix
+                .endif
+                load-module module-native-protocol-unix
+
+                ## JACK support
+                .ifexists module-jackdbus-detect.so
+                load-module module-jackdbus-detect channels=2
+                .endif
+
+                ## Bluetooth support
+                .ifexists module-bluetooth-policy.so
+                load-module module-bluetooth-policy
+                .endif
+                .ifexists module-bluetooth-discover.so
+                load-module module-bluetooth-discover
+                .endif
+
+                ## If you want Internet support, add it here.
+
+                ## GSettings support
+                ## You can configure these via paprefs; but doing so may cause conflicts with manually-loaded modules.
+                .ifexists module-gsettings.so
+                .nofail
+                load-module module-gsettings
+                .fail
+                .endif
+
+                ## Ensure there is always at least one sink, even if it's null.
+                ## Should run after all other sink-loading commands have run.
+                load-module module-always-sink
+
+                ################################################################################
+                ## C O N F I G U R E   S I N K S                                              ##
+                ################################################################################
+
+                ## Restore default sink
+                ## Should be as early as possible so that as many modules as possible have the correct default sink.
+                load-module module-default-device-restore
+
+                ## Rescue streams (obsolete)
+                # load-module module-rescue-streams
+
+                ## Suspend idle sinks
+                load-module module-suspend-on-idle
+
+                ## Ensure `autoexit` doesn't kill PulseAudio mid-session
+                .ifexists module-console-kit.so
+                load-module module-console-kit
+                .endif
+                .ifexists module-systemd-login.so
+                load-module module-systemd-login
+                .endif
+
+                ################################################################################
+                ## E F F E C T S                                                              ##
+                ################################################################################
+
+                ## Positionalize event sounds
+                load-module module-position-event-sounds
+
+                ## Loads filters (like echo cancellation) on-demand
+                load-module module-filter-heuristics
+                load-module module-filter-apply
+
+                ## Cork A/V streams when a phone stream is active
+                load-module module-role-cork
+
+                ## Block recording for snaps that don't plug the "pulseaudio"/"audio-record" interfaces
+                .ifexists module-snap-policy.so
+                load-module module-snap-policy
+                .endif
+
+                ################################################################################
+
+                ## Includes the `default.pa.d`, where additional configs may lie.
+                .nofail
+                .include /etc/pulse/default.pa.d
+            ''
+            daemon.config = {
+
+                ## Memory
+                lock-memory = true; ## Prevents paging out audio-related memory.
+                enable-memfd = true; ## Modern memory architecture.
+                enable-shm = true; ## Legacy memory architecture. Used if memfd isn't available.
+                # shm-size-bytes = 0; ## 0 == auto (64M)
+
+                ## Scheduling
+                cpu-limit = false;
+                high-priority = true; ## Ignored if RT.
+                nice-level = -19; ## Ignored if RT.
+                realtime-scheduling = true;
+                realtime-priority = 40;
+                default-fragments = 2; ## Set to the lowest value that doesn't give you playback issues.
+                default-fragment-size-msec = 4; ## Going too far below about 5 can cause issues with quality (metalicness, ringing). `4`, `8`, and `17` allows you to make your total latency be some rough multiple of fps.
+                enable-deferred-volume = true; ## Avoids harsh changes mid-buffer.
+                deferred-volume-safety-margin-usec = 3000; ## Must be large-enough to handle timing errors, yet smaller than default-fragment-size-msec.
+                deferred-volume-extra-delay-usec = 0; ## Should be 0 unless you're working around hardware issues.
+                scache-idle-time = 60; ## How long to cache audio. Reduces disk reads.
+                exit-idle-time = 1; ## Affects `avoid-resampling`; should be low so that content plays more-often in its native rate. Consider adding a 1s delay between tracks in your audio player to take full advantage of automatic sample-rate switching.
+
+                ## Sampling
+                avoid-resampling = true; ## Locks Pulseaudio into the sample rate of the first track it plays.
+                default-sample-format = "float32le"; ## Not using 32+ bits or floats means deleting entire bits when scaling volume.
+                default-sample-rate = 48000; ## Best conventional option for math reasons.
+                alternate-sample-rate = 44100; ## CD-quality audio. Has to be supported because it's extremely common.
+                resample-method = "soxr-vhq";
+
+                ## Channels
+                enable-remixing = true;
+                remixing-use-all-sink-channels = true; ## Ensures surround-sound sats aren't sitting there idle during stereo playback.
+                remixing-produce-lfe = false; ## Keep bass stereo and let the speakers sort it out.
+                remixing-consume-lfe = true;
+                lfe-crossover-freq = 120; ## 120Hz is the official standard for LFE XO; we have to match it for playback to be accurate. The only reason to break the 120Hz standard is when you're generating your own LFE channel, which we aren't doing.
+                default-sample-channels = 2;
+                # default-channel-map = "side-left,side-right"; ## Whether speakers or headphones, I always have my sources at 180°. With `enable-remixing = true`, front channels (99% of everything) should move to side without issue, while surround will remap more-gracefully than it would to front.
+                default-channel-map = "front-left,front-right" ## The canonical option. Ensures nothing breaks, but not as literally correct.
+
+                ## Misc
+                rescue-streams = true; ## Prevents streams from sticking with dead sinks.
+                flat-volumes = false; ## Misguided behavior; leave disabled.
+
+                ## Logging
+                log-target = "auto";
+                log-level = "notice";
+                log-meta = false;
+                log-time = true;
+                log-backtrace = 0;
+            };
+        };
     };
 
     ## Settings: OpenAL
