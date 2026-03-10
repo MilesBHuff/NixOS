da79187beec487cab4dcdf3fad0b4d85db8cd6bd
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2026-03-10 00:56:14 -0400

    Removed BT role restrictions.

--- nixos/subsystems/audio/service.nix
+++ nixos/subsystems/audio/service.nix
@@ -53,13 +53,12 @@ in {
         jack.enable = true;
 
         wireplumber.extraConfig = {
-            ## Copied from: https://nixos.wiki/wiki/PipeWire#Bluetooth_Configuration
+            ## Modified from: https://nixos.wiki/wiki/PipeWire#Bluetooth_Configuration
             bluetoothEnhancements = {
                 "monitor.bluez.properties" = {
                     "bluez5.enable-sbc-xq" = true;
                     "bluez5.enable-msbc" = true;
                     "bluez5.enable-hw-volume" = true;
-                    "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ]; ## Try removing if you encounter weird issues.
                 };
             };
 
