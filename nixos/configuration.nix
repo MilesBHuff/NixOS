e7cd12d855c24bf5c387237e3114586f2e1440a7
Miles B Huff <MilesBHuff@users.noreply.github.com>
2026-07-15 12:38:53 -0500

    Removed unneeded quotes.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,31 +2,31 @@
 {config, pkgs, lib, var, ...}: {
     system.stateVersion = "24.05"; ## The version of NixOS you are using.
     imports = [
-        "./initializations.nix"
-        "./hardware-configuration.nix" ## Run `nixos-generate-config` to update this file.
+        ./initializations.nix
+        ./hardware-configuration.nix ## Run `nixos-generate-config` to update this file.
 
-        "./subsystems/localization.nix"
-        "./subsystems/time.nix"
-        "./subsystems/networking.nix"
-        "./subsystems/audio.nix"
-        "./subsystems/printing.nix"
-        "./subsystems/desktop.nix"
-        "./subsystems/packages.nix"
+        ./subsystems/localization.nix
+        ./subsystems/time.nix
+        ./subsystems/networking.nix
+        ./subsystems/audio.nix
+        ./subsystems/printing.nix
+        ./subsystems/desktop.nix
+        ./subsystems/packages.nix
 
-        "./startup/format.nix"
-        "./startup/mounts.nix" ## Overrides `hardware-configuration.nix`.  Comment to not override.
-        "./startup/boot.nix"
+        ./startup/format.nix
+        ./startup/mounts.nix ## Overrides `hardware-configuration.nix`.  Comment to not override.
+        ./startup/boot.nix
 
-        "./users/miles.nix"
+        ./users/miles.nix
 
-        "./actions/hibernate.nix"
-        "./actions/restart.nix"
+        ./actions/hibernate.nix
+        ./actions/restart.nix
 
-        "./units/antivirus.nix"
-        "./units/defragment.nix"
-        "./units/rotational-settings.nix"
-        "./units/scrub.nix"
-        "./units/snapshots.nix"
-        "./units/swap.nix"
+        ./units/antivirus.nix
+        ./units/defragment.nix
+        ./units/rotational-settings.nix
+        ./units/scrub.nix
+        ./units/snapshots.nix
+        ./units/swap.nix
     ];
 }
